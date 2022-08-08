d57a8e3be318b680166fe60174800f6e7ad569b0
Vaishak Nair <vaishak.web@gmail.com>
2022-08-08 09:00:09 -0700

    map and list section

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ const App = () => {
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
-              <Grid item xs={12} md={4}>
+        <Grid item xs={12} md={4}>
           <List />
         </Grid>
         <Grid item xs={12} md={8}>
-          <Map />
+            <Map />
         </Grid>
       </Grid>
     </>
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -23,7 +23,7 @@ const Header = () => {
                             <div className={classes.searchIcon}>
                                 <SearchIcon />
                             </div>
-                            <InputBase placeholder='Search...' classes={{root:classes.inputRoot, input:classes.inputInput}}></InputBase>
+                            <InputBase placeholder='Search...' classes={{root:classes.inputRoot, input:classes.inputInput}} />
                         </div>
                     {/* </Autocomplete> */}
                 </Box>
