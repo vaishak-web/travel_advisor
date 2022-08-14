758a360d9463b18f3df334484b0ccd6fbbc005a7
Vaishak Nair <vaishak.web@gmail.com>
2022-08-14 04:00:36 -0700

    change map setting

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -15,7 +15,7 @@ const Map = ({ setCoordinates, setBounds, coordinates, places, setchildClicked})
         bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_API_KEY }}
         defaultCenter={coordinates}
         center={coordinates}
-        defaultZoom={12}
+        defaultZoom={0}
         margin={[50, 50, 50, 50]}
         options={""}
         onChange={(e) => {
